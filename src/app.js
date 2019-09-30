f38a6b452ee086771562f7809cf325055374124c
Karina Gaulin <karina@domandtom.com>
2019-09-30 09:42:46 -0400

    Initial commit

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+app.post('/', (req, res) => {
+  res.send('POST request received.')
 })
 
 app.use(function errorHandler(error, req, res, next) {
